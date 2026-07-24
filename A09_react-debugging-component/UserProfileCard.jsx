9ab8232a46eaff5cc69e92dd16f9323502a7df48
alix-8 <bonalexandrian@gmail.com>
2026-07-24 15:56:50 +0800

    Done and Completed Part B

---
+++ A09_react-debugging-component/UserProfileCard.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+const userData = {
+  name: "Alexandrian Bon",
+  avatarUrl: "https://avatars.githubusercontent.com/u/188729846?v=4", // sample avatar
+  bio: "BSIS 3 Student",
+  skills: ["React", "Python", "Java", "JavaScript", "HTML", "CSS"],
+  isOnline: true,
+  lastUpdated: "2 hours ago",
+};
+
+//<UserProfileCard user={userData} />;
+
+function UserProfileCard({ user }) {
+  const [messageCount, setMessageCount] = useState(0);
+  const [isFavorited, setIsFavorited] = useState();
+
+  function handleSendMessage() {
+    setMessageCount(messageCount + 1);
+  }
+
+  function handleReset() {
+    setMessageCount(0);
+  }
+
+  return (
+    <>
+      <div className="profile-card">
+        <img src={userData.avatarUrl} />
+        <h2>{userData.name}</h2>
+        <label htmlFor="bio">Bio</label>
+        <p id="bio">{userData.bio}</p>
+        <h3>Skills</h3>
+        <ul>
+          {userData.skills.map((skill) => (
+            <li key={skill}>{skill}</li>
+          ))}
+        </ul>
+        <div style={{ color: "blue", fontWeight: "bold" }}>
+          Messages sent: {messageCount}
+        </div>
+        {userData.isOnline ? <span>🟢 Online</span> : <span>⚪ Offline</span>}
+        <button onClick={handleSendMessage}>Send Message</button>
+        <button onClick={handleReset}>Reset</button>
+      </div>
+      <p className="footer"> Card last updated: {userData.lastUpdated} </p>
+    </>
+  );
+}
+
+export default UserProfileCard;
